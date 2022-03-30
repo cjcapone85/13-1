3a8325b298af137637cd1692860da245046bcfd8
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-03-30 12:23:08 -0500

    Class 13-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'property-binding-image-size';
+width: number = 200;
+height: number = 200;
 }
